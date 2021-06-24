c17a576fef547c8f3eeaf85291f2020f054e931d
Filip Katusin <filip.katusin@gmail.com>
2021-06-24 12:16:00 +0200

    pridanie linkov na nove projekty

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import Card from './Card'
-import '../css/projects.css'
-import data from '../projects-links'
+import React from "react";
+import Card from "./Card";
+import "../css/projects.css";
+import data from "../projects-links";
+
+import Fade from "react-reveal/Fade";
 
 function Projects() {
+  return (
+    <div id="projects-container">
+      <h2>These are some of my projects</h2>
 
-    return (
-        <div id="projects-container">
-            <h2>These are some of my projects</h2>
-
-            <div id="projects">
-                {
-                    data.map((info,index) => {
-                        return <Card info={info} key={index} />
-                    })
-                }
-            </div>
-            
-        </div>
-    )
+      <div id="projects">
+        {data.map((info, index) => {
+          return (
+            // <Fade className="project">
+            <Card info={info} key={index} />
+            // </Fade>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/projects-links.js
+++ src/projects-links.js
@@ -1,28 +1,44 @@
 const data = [
-    {
-        name: 'Todo list',
-        img: 'todo-list.JPG',
-        link: 'https://todo-list175.netlify.app'
-    },
-    {
-        name: 'Colors generator',
-        img: 'colors-generator.JPG',
-        link: 'https://nifty-wescoff-50f6d6.netlify.app'
-    },
-    {
-        name: 'Shopping card',
-        img: 'shopping-card.JPG',
-        link: 'https://infallible-panini-57e036.netlify.app'
-    },{
-        name: 'Landing page',
-        img: 'landing-page.JPG',
-        link: 'https://insure-landing-page-alpha-azure.vercel.app/'
-    },
-    {
-        name: 'Pricing card',
-        img: 'pricing.JPG',
-        link: 'https://interactive-pricing-nine.vercel.app/'
-    }
-]
+  {
+    name: "Countries search API",
+    img: "countries-search.png",
+    link: "https://countries-search-api.netlify.app/",
+  },
+  {
+    name: "Easybank landing page",
+    img: "easybank.png",
+    link: "https://easybank-katusin.netlify.app/",
+  },
+  {
+    name: "Calculator",
+    img: "calculator.png",
+    link: "https://calculator-katusin.netlify.app/",
+  },
+  {
+    name: "Chair landing page",
+    img: "chair-landing-page.png",
+    link: "https://chair-landing-page.netlify.app/",
+  },
+  {
+    name: "Todo list",
+    img: "todo-list.JPG",
+    link: "https://todo-list175.netlify.app",
+  },
+  {
+    name: "Shopping card",
+    img: "shopping-card.JPG",
+    link: "https://infallible-panini-57e036.netlify.app",
+  },
+  {
+    name: "Landing page",
+    img: "landing-page.JPG",
+    link: "https://insure-landing-page-alpha-azure.vercel.app/",
+  },
+  {
+    name: "Pricing card",
+    img: "pricing.JPG",
+    link: "https://interactive-pricing-nine.vercel.app/",
+  },
+];
 
 export default data;
