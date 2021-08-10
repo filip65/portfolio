58c1efb0b1457643db2d631f68feae920aac082f
Filip Katusin <filip.katusin@gmail.com>
2021-08-10 15:00:39 +0200

    pridanie fotky pre blog a zmena linku na todo

--- src/projects-links.js
+++ src/projects-links.js
@@ -37,23 +37,23 @@ const data = [
   {
     name: "Todo list",
     img: "todo-list.JPG",
-    link: "https://todo-list175.netlify.app",
+    link: "https://todolist324.netlify.app/",
   },
-  {
-    name: "Shopping card",
-    img: "shopping-card.JPG",
-    link: "https://infallible-panini-57e036.netlify.app",
-  },
-  {
-    name: "Landing page",
-    img: "landing-page.JPG",
-    link: "https://insure-landing-page-alpha-azure.vercel.app/",
-  },
-  {
-    name: "Pricing card",
-    img: "pricing.JPG",
-    link: "https://interactive-pricing-nine.vercel.app/",
-  },
+  // {
+  //   name: "Shopping card",
+  //   img: "shopping-card.JPG",
+  //   link: "https://infallible-panini-57e036.netlify.app",
+  // },
+  // {
+  //   name: "Landing page",
+  //   img: "landing-page.JPG",
+  //   link: "https://insure-landing-page-alpha-azure.vercel.app/",
+  // },
+  // {
+  //   name: "Pricing card",
+  //   img: "pricing.JPG",
+  //   link: "https://interactive-pricing-nine.vercel.app/",
+  // },
 ];
 
 export default data;
