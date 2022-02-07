7dfd79108ad6a3172af6c008acf6fa443c923840
filip65 <filip.katusin@gmail.com>
2022-02-07 09:22:33 +0100

    hover na social link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,51 @@
-import React from 'react'
-import '../css/footer.css'
-import {AiOutlineFacebook, AiOutlineLinkedin, AiOutlineMail} from 'react-icons/ai';
+import React from "react";
+import "../css/footer.css";
+import {
+  AiOutlineLinkedin,
+  AiOutlineMail,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+import { FaBeer } from "react-icons/fa";
 
 function Footer() {
-    return (
-        <div id="footer">
-            <h2>Feel free to contact me!</h2>
-            <p>Better every day.</p>
-                <ul className="links">
-                    <li className="profile-link">
-                        <a href="https://www.facebook.com/filip.katusin/" target="_blank" rel="noreferrer">
-                            <AiOutlineFacebook className="socialIcon"/>
-                            <p>Facebook</p>
-                        </a>
-                    </li>
-                    <li className="profile-link" id="profile-link">
-                        <a href="https://www.linkedin.com/in/filip-katu%C5%A1in-09949490/" target="_blank" rel="noreferrer">
-                            <AiOutlineLinkedin className="socialIcon" />
-                            <p>Linkedin</p>
-                        </a>
-                    </li>
-                    <li className="profile-link">
-                        <a href="mailto:filip.katusin@gmail.com" target="_blank" rel="noreferrer">
-                            <AiOutlineMail className="socialIcon"/>
-                            <p>Email</p>
-                        </a>
-                    </li>
-                </ul>
-        </div>
-    )
+  return (
+    <div id="footer">
+      <h2>Feel free to contact me!</h2>
+      <p>Better every day.</p>
+      <ul className="links">
+        <li className="profile-link">
+          <a
+            href="https://www.facebook.com/filip.katusin/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineFacebook className="icon" />
+            <p>Facebook</p>
+          </a>
+        </li>
+        <li className="profile-link" id="profile-link">
+          <a
+            href="https://www.linkedin.com/in/filip-katu%C5%A1in-09949490/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineLinkedin className="icon" />
+            <p>Linkedin</p>
+          </a>
+        </li>
+        <li className="profile-link">
+          <a
+            href="mailto:filip.katusin@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineMail className="icon" />
+            <p>Email</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
